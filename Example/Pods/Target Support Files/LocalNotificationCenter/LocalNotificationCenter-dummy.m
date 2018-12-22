1e01b5a6fb989a7382eeabdc4eb2991248eedcb4
Christian Gossain <cgossain@gmail.com>
2018-12-22 13:10:50 -0700

    Initial pod library commit

---
+++ Example/Pods/Target Support Files/LocalNotificationCenter/LocalNotificationCenter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LocalNotificationCenter : NSObject
+@end
+@implementation PodsDummy_LocalNotificationCenter
+@end
---
+++ Example/Pods/Target Support Files/Pods-LocalNotificationCenter_Example/Pods-LocalNotificationCenter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LocalNotificationCenter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LocalNotificationCenter_Example
+@end
