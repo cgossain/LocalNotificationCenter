7a74968428fb5241c3b95965176cde1147330a85
Christian Gossain <cgossain@gmail.com>
2018-12-23 17:14:40 -0700

    Rewrite LocalNotificationCenter to be based on UserNotifications framework

--- LocalNotificationCenter.podspec
+++ LocalNotificationCenter.podspec
@@ -1,18 +1,15 @@
 Pod::Spec.new do |s|
   s.name             = 'LocalNotificationCenter'
-  s.version          = '1.0.0'
-  s.summary          = 'A lightweight library for scheduling local notifications on iOS.'
+  s.version          = '0.2.0'
+  s.summary          = 'A lightweight library for scheduling local notifications on iOS, written in Swift.'
   s.description      = <<-DESC
 A lightweight library for scheduling local notifications on iOS.
                        DESC
-
   s.homepage         = 'https://github.com/cgossain/LocalNotificationCenter'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'cgossain' => 'cgossain@gmail.com' }
   s.source           = { :git => 'https://github.com/cgossain/LocalNotificationCenter.git', :tag => s.version.to_s }
-
   s.ios.deployment_target = '10.3'
-
   s.source_files = 'LocalNotificationCenter/Classes/**/*'
   s.framework    = 'UserNotifications'
 end
--- LocalNotificationCenter/Classes/LocalNotificationCenter.swift
+++ LocalNotificationCenter/Classes/LocalNotificationCenter.swift
@@ -9,94 +9,124 @@
 import Foundation
 import UserNotifications
 
-open class LocalNotificationCenter: NSObject {
+/// The LocalNotificationCenter is a light wrapper around the UserNotifications center framework, and only keeps
+/// track of only local notifications scheduled by itself.
+public class LocalNotificationCenter: NSObject {
     private struct UserInfoKeys {
-        static let notificationKey = "LocalNotificationCenterNotificationKey"
+        static let notificationIdentifier = "LocalNotificationCenterNotificationKey"
     }
     
     /// The default notification center instance.
     public static let `default` = LocalNotificationCenter()
     
-    /// All local notifications that have been scheduled through the notification center.
-    open var scheduledLocalNotificationsByKey: [String : UILocalNotification] {
-        return mutableScheduledLocalNotificationsByKey
+    /// A list of all notification requests (managed by the receiver) that are scheduled and waiting to be delivered.
+    public var pendingNotificationRequestsByIdentifier: [String : UNNotificationRequest] {
+        return mutablePendingNotificationRequestsByIdentifier
     }
     
-    fileprivate var mutableScheduledLocalNotificationsByKey = [String : UILocalNotification]()
+    /// A mutable list of all notification requests (managed by the receiver) that are scheduled and waiting to be delivered.
+    fileprivate var mutablePendingNotificationRequestsByIdentifier: [String : UNNotificationRequest] = [:]
     
+    
+    // MARK: - Lifecycle
     override init() {
         super.init()
         loadScheduledLocalNotifications()
     }
     
-    fileprivate func loadScheduledLocalNotifications() {
-        if let scheduledLocalNotifications = UIApplication.shared.scheduledLocalNotifications {
-            for notification in scheduledLocalNotifications {
-                if let key = notification.userInfo?[LocalNotificationCenterNotificationKey] as? String {
-                    // this notification was scheduled by us, so let's track it
-                    mutableScheduledLocalNotificationsByKey[key] = notification
-                }
+    /// Schedules a local notification with the given parameters.
+    ///
+    /// - parameters:
+    ///     - identifier: The unique identifier for this notification.
+    ///     - body: The body of the notification alert.
+    ///     - dateMatching: The date components for which to fire the notification on.
+    ///     - repeats: A Boolean value indicating whether the system reschedules the notification after it is delivered.
+    ///     - userInfo: A dictionary of custom information associated with the notification.
+    public func scheduleLocalNotification(withIdentifier identifier: String,
+                                          body: String,
+                                          dateMatching dateComponents: DateComponents,
+                                          repeats: Bool,
+                                          userInfo: [String : AnyObject]?) {
+        // ignore if the notification is already scheduled
+        if isLocalNotificationScheduled(forIdentifier: identifier) {
+            return
+        }
+        
+        // add the given user info to the notification content user info
+        var notificationContentUserInfo: [AnyHashable : Any] = [LocalNotificationCenter.UserInfoKeys.notificationIdentifier : identifier]
+        userInfo?.forEach({ notificationContentUserInfo[$0.key] = $0.value })
+        
+        // create the local notification content
+        let content = UNMutableNotificationContent()
+        content.body = body
+        content.userInfo = notificationContentUserInfo
+        
+        // create the notification trigger
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: repeats)
+        
+        // create a notification request
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        // add the notification request
+        UNUserNotificationCenter.current().add(request) { [weak self] (error) in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            if error == nil {
+                // track the scheduled notification request
+                strongSelf.mutablePendingNotificationRequestsByIdentifier[identifier] = request
             }
         }
     }
     
     /// Returns true if a local notification is scheduled for the given key.
-    open func isLocalNotificationScheduled(forKey key: String) -> Bool {
-        return (mutableScheduledLocalNotificationsByKey[key] != nil)
+    public func isLocalNotificationScheduled(forIdentifier identifier: String) -> Bool {
+        return mutablePendingNotificationRequestsByIdentifier[identifier] != nil
     }
     
     /// Cancels the local notification associated with the given key if it is scheduled.
-    open func cancelLocalNotification(forKey key: String) {
-        if let notification = mutableScheduledLocalNotificationsByKey[key] {
-            UIApplication.shared.cancelLocalNotification(notification)
-            mutableScheduledLocalNotificationsByKey[key] = nil
+    public func cancelScheduledLocalNotification(forIdentifier identifier: String) {
+        // ignore if we did not schedule a notification for this identifier
+        if !isLocalNotificationScheduled(forIdentifier: identifier) {
+            return
         }
+        
+        // remove a pending request for the given identifer from the notification center
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [identifier])
+        
+        // untrack a pending request for the given identifer
+        mutablePendingNotificationRequestsByIdentifier[identifier] = nil
     }
     
     /// Cancels all scheduled local notifications.
-    open func cancelAllLocalNotifications() {
-        UIApplication.shared.cancelAllLocalNotifications()
-        mutableScheduledLocalNotificationsByKey.removeAll()
+    public func cancelAllScheduledLocalNotifications() {
+        // gather all identifiers scheduled by us
+        let identifiersToRemove = Array(pendingNotificationRequestsByIdentifier.keys)
+        
+        // remove all pending requests that were scheduled by us
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifiersToRemove)
+        
+        // untrack all pending requests
+        mutablePendingNotificationRequestsByIdentifier.removeAll()
     }
     
-    /// Schedules a local notification with the given parameters.
-    ///
-    /// - parameter on: The fire date for the local notification. Pass nil to fire the notification immediately.
-    ///
-    @discardableResult
-    open func scheduleNotification(_ on: Date?, forKey key: String, alertBody: String?, alertAction: String?, soundName: String?, launchImage: String?, userInfo: [String : AnyObject]?, repeatInterval: NSCalendar.Unit) -> UILocalNotification? {
-        // ignore if the notification is already scheduled
-        if isLocalNotificationScheduled(forKey: key) {
-            return nil
-        }
-        
-        // store the key in the notification user info and add additional user keys
-        var notificationUserInfo: [String : AnyObject] = [LocalNotificationCenter.UserInfoKeys.notificationKey : key as AnyObject]
-        if let userInfo = userInfo {
-            for (key, value) in userInfo {
-                notificationUserInfo[key] = value
+}
+
+fileprivate extension LocalNotificationCenter {
+    func loadScheduledLocalNotifications() {
+        UNUserNotificationCenter.current().getPendingNotificationRequests { [weak self] (requests) in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            // load only notification requests created by the LocalNotificationCenter class (based on existence of key in userInfo)
+            for request in requests {
+                guard let identifier = request.content.userInfo[UserInfoKeys.notificationIdentifier] as? String else {
+                    continue
+                }
+                strongSelf.mutablePendingNotificationRequestsByIdentifier[identifier] = request
             }
         }
-        
-        // create the local notification
-        let localNotification = UILocalNotification()
-        localNotification.userInfo              = notificationUserInfo
-        localNotification.fireDate              = on
-        localNotification.timeZone              = TimeZone.current
-        localNotification.alertBody             = alertBody
-        localNotification.alertAction           = alertAction
-        localNotification.soundName             = soundName ?? UILocalNotificationDefaultSoundName
-        localNotification.alertLaunchImage      = launchImage
-        localNotification.repeatInterval        = repeatInterval
-        
-        // remember the notification
-        mutableScheduledLocalNotificationsByKey[key] = localNotification
-        
-        // schedule the notification
-        UIApplication.shared.scheduleLocalNotification(localNotification)
-        
-        // return the notification
-        return localNotification
     }
-    
 }
